7aa1f088ac7d7131eacac0671f1e7b21b5af6340
okayvid <misc@watermelon4468.anonaddy.me>
2022-08-23 08:12:45 +0100

    More blog post tweaking

--- articles.php
+++ articles.php
@@ -1,10 +1,19 @@
 <?php require_once( 'couch/cms.php' ); ?>
+<cms:template title="Add a new article" clonable="1">
+    <cms:editable name="article_content" type="richtext" />
+</cms:template>
+<cms:if k_is_page >
 <!doctype html>
 <html>
     <body>
-        <div style="background-color:grey:">
-            <h1>test simple content</h1>
+        <div style="background-color:grey;">
+            <h1><cms:show k_page_title /></h1>
+            <h3><cms:date k_page_date format="jS M, y"/></h3>
+            <cms:show article_content />
         </div>
     </body>
 </html>
+<cms:else />
+    <cms:embed 'articles.php' />
+</cms:if>
 <?php COUCH::invoke(); ?>

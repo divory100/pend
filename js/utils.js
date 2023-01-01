b55c5e8814321eee6b4182f278fa4b644f9874ec
okayvid <misc@watermelon4468.anonaddy.me>
2023-01-01 19:21:25 +0000

    filtering by category works, only added to read.php so far

--- js/utils.js
+++ js/utils.js
@@ -1,5 +1,32 @@
 function applyListFilter() {
     //called whenever a user presses "Filter" on one of the list view filter forms.
     //Will produce a url with couchcms filtering parameters and redirect the user to that url.
-    console.log(document.getElementById("select-folder").value);
+    let category = document.getElementById('select-folder').value;
+    let contentType = document.getElementById('select-contenttype').value;
+
+    if (category == '' && contentType == '') {
+        //no filter selected
+        return;
+    }
+    
+    //current url (doing it this way removes parameters)
+    var new_url = window.location.protocol + '//' + window.location.host + window.location.pathname;
+
+    //a category filter has been selected
+    if (category != '') {
+        new_url = new_url + '?f=' + category;
+    }
+
+    //a content type filter has been selected (can be both)
+    if (contentType != '') {
+
+    }
+
+    //redirect the user
+    window.location = new_url;
+}
+
+function resetListFilter() {
+    //strip parameters and redirect
+    window.location = window.location.protocol + '//' + window.location.host + window.location.pathname;
 }
